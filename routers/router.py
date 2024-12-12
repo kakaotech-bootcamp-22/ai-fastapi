77fd50d3834d9a57efcb7c2a673e39bc23bf4c40
Meedam Byun <81531278+midamidaa@users.noreply.github.com>
2024-12-12 11:08:13 +0900

    Update router.py

--- routers/router.py
+++ routers/router.py
@@ -20,7 +20,9 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument('--disable-gpu')
-driver = webdriver.Chrome(options=chrome_options)
+# ChromeDriverManager 대신 시스템에 설치된 ChromeDriver 사용
+service = Service('/usr/local/bin/chromedriver')
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
 
 # chrome_options = Options()
